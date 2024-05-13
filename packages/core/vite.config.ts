ceec56dbd7d00f4b6ffcc7bf8337333579ff4474
Kiran Gadhave <kirangadhave2@gmail.com>
2024-05-13 13:50:33 -0600

    chore: [nx migration] update-vite-config

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -6,6 +6,7 @@ import dts from 'vite-plugin-dts';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
+    root: __dirname,
     cacheDir: '../../node_modules/.vite/core',
     define: {
         'process.env': {},
@@ -32,6 +33,9 @@ export default defineConfig({
     // Configuration for building your library.
     // See: https://vitejs.dev/guide/build.html#library-mode
     build: {
+        outDir: '../../dist/packages/core',
+        reportCompressedSize: true,
+        commonjsOptions: { transformMixedEsModules: true },
         sourcemap: true,
         lib: {
             // Could also be a dictionary or array of multiple entry points.
@@ -56,6 +60,11 @@ export default defineConfig({
     },
 
     test: {
+        reporters: ['default'],
+        coverage: {
+            reportsDirectory: '../../coverage/packages/core',
+            provider: 'v8',
+        },
         globals: true,
         cache: {
             dir: '../../node_modules/.vitest',
--- packages/redux/vite.config.ts
+++ packages/redux/vite.config.ts
@@ -6,6 +6,7 @@ import dts from 'vite-plugin-dts';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
+    root: __dirname,
     cacheDir: '../../node_modules/.vite/redux',
     define: {
         'process.env': {},
@@ -32,6 +33,9 @@ export default defineConfig({
     // Configuration for building your library.
     // See: https://vitejs.dev/guide/build.html#library-mode
     build: {
+        outDir: '../../dist/packages/redux',
+        reportCompressedSize: true,
+        commonjsOptions: { transformMixedEsModules: true },
         sourcemap: true,
         lib: {
             // Could also be a dictionary or array of multiple entry points.
@@ -57,6 +61,11 @@ export default defineConfig({
     },
 
     test: {
+        reporters: ['default'],
+        coverage: {
+            reportsDirectory: '../../coverage/packages/redux',
+            provider: 'v8',
+        },
         globals: true,
         cache: {
             dir: '../../node_modules/.vitest',
